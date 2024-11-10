27489cd62c59254d22a0b22864a18dbe65709ee6
ZechariahP <162992033+ZechariahP@users.noreply.github.com>
2024-11-10 17:35:42 +0200

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import MainPage from './components/MainPage';
 import './App.css';
 
 const App: React.FC = () => {
+  const isAuthenticated = !!localStorage.getItem('user'); // Check if the user is authenticated
+
   return (
     <Router>
       <Routes>
-        <Route path="*" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<MainPage />} />
+        <Route
+          path="/"
+          element={isAuthenticated ? <MainPage /> : <Navigate to="/login" />}
+        />
+        <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect to login page by default */}
       </Routes>
     </Router>
   );
