da4611d908db73da6a68ecd86aacbc970a89db86
ZechariahP <162992033+ZechariahP@users.noreply.github.com>
2024-11-10 19:22:31 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import MainPage from './components/MainPage';
 import './App.css';
 
 const App: React.FC = () => {
+
   return (
     <Router>
       <Routes>
+        <Route path="*" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect to login page by default */}
+        <Route path="/" element={<MainPage />} />
       </Routes>
     </Router>
   );
